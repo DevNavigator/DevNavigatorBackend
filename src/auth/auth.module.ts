7b389e7fa111b222b542263cdb177c3742b62f4e
John Di Donna <139842867+DiDonna29@users.noreply.github.com>
2024-11-12 00:23:19 -0400

    sistema de puntuacion de usuarios

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { User } from 'src/user/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from 'src/user/user.repository';
 import { EmailModule } from 'src/email/email.module';
+import { StatisticsModule } from 'src/statistics/statistics.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), EmailModule],
+  imports: [TypeOrmModule.forFeature([User]), EmailModule, StatisticsModule],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
 })
